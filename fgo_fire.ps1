10bbd7e18dc5184c7b5a7d27fb7d848dc88bdae2
kenshinn <kenshinnn@gmail.com>
2018-02-27 15:35:32 +0800

    Create fgo_fire.ps1

---
+++ fgo_fire.ps1
@@ -0,0 +1,73 @@
+#等待進入戰鬥
+sleep 35
+#孔明3技
+adb shell input tap 1777 1160
+sleep 3
+#孔明2技
+adb shell input tap 1600 1160
+sleep 3
+#進入攻擊
+adb shell input tap 2273 1190
+sleep 1
+#寶具1
+adb shell input tap 822 422
+Start-sleep -Milliseconds 500
+#角色1
+adb shell input tap 250 1000
+Start-sleep -Milliseconds 500
+#角色2
+adb shell input tap 750 1000
+sleep 35
+#孔明1技
+adb shell input tap 1420 1160
+Start-sleep -Milliseconds 500
+#選角色1
+adb shell input tap 650 850
+sleep 3
+#master 技能
+adb shell input tap 2400 625
+Start-sleep -Milliseconds 500
+#master 技能3
+adb shell input tap 2180 625
+sleep 1
+#選角色3
+adb shell input tap 1070 715
+Start-sleep -Milliseconds 500
+#選角色5
+adb shell input tap 1850 715
+Start-sleep -Milliseconds 500
+#確認換人
+adb shell input tap 1280 1250
+sleep 10
+#孔明3技
+adb shell input tap 1777 1160
+sleep 3
+#孔明2技
+adb shell input tap 1600 1160
+sleep 3
+#玉澡貓1技
+adb shell input tap 150 1160
+sleep 3
+#進入攻擊
+adb shell input tap 2273 1190
+sleep 2
+#寶具3
+adb shell input tap 1300 422
+Start-sleep -Milliseconds 500
+#角色1
+adb shell input tap 250 1000
+Start-sleep -Milliseconds 500
+#角色2
+adb shell input tap 750 1000
+sleep 30
+#進入攻擊
+adb shell input tap 2273 1190
+sleep 2
+#寶具2
+adb shell input tap 850 422
+Start-sleep -Milliseconds 500
+#角色1
+adb shell input tap 250 1000
+Start-sleep -Milliseconds 500
+#角色2
+adb shell input tap 750 1000
